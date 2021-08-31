dc78448c48decf74b61f7ef0fa926366e72ac199
timh <tim@tim.hansen.se>
2021-08-31 16:22:26 +0200

    styled nav

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,21 +1,24 @@
 import { Link } from 'react-router-dom';
 import imgLogga from '../../images/loggor/logga.png';
-import './navbar.css';
+import Styles from './navbar.module.css';
 
 const Navbar = () => {
   return (
-    <div className="wrapper">
-      <nav className="navbar">
-        <img src={imgLogga} alt="" className="logo" />
-        <div className="links">
-          <Link to="/">Om oss</Link>
-          <Link to="/">Kontakt</Link>
-          <Link to="/">Vanliga frågor</Link>
+    <div className={Styles.wrapper}>
+      <nav className={Styles.content}>
+        <img src={imgLogga} alt="" className={Styles.logo} />
+        <div className={Styles.links}>
+          <Link to="/">About</Link>
+          <Link to="/">Contact</Link>
+          <Link to="/">FAQ</Link>
           <Link to="/">Login</Link>
           <Link to="/">Sign up</Link>
         </div>
-        <button>SV</button>
-        <button>EN</button>
+
+        <div className={Styles.langBtns}>
+          <button className={Styles.svBtn}>SV</button>
+          <button className={Styles.enBtn}>EN</button>
+        </div>
       </nav>
     </div>
   );
