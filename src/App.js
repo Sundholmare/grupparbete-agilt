b6a172a1c673b6d00313044517ba2005dda7a6b1
timh <tim@tim.hansen.se>
2021-09-01 12:14:18 +0200

    fixed routing

--- src/App.js
+++ src/App.js
@@ -3,18 +3,24 @@ import Navbar from './components/Navbar';
 import About from './components/about/index';
 import Hero from './components/Hero';
 import Offer from './components/Offer';
-import Footer from './components/Footer'
-
+import Footer from './components/Footer';
 
 function App() {
   return (
     <Router>
-      <div className="main-wrapper">
-        <div className="App">
-          <Navbar />
-          <Hero />
-          <Offer />
-          <Footer />
+      <div className="App">
+        <Navbar />
+        <div className="main-wrapper">
+          <Switch>
+            <Route exact path="/">
+              <Hero />
+              <Offer />
+              <Footer />
+            </Route>
+            <Route path="/about">
+              <About />
+            </Route>
+          </Switch>
         </div>
       </div>
     </Router>
--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -8,11 +8,11 @@ const Navbar = () => {
       <nav className={Styles.content}>
         <img src={imgLogga} alt="" className={Styles.logo} />
         <div className={Styles.links}>
-          <Link to="/">ABOUT</Link>
-          <Link to="/">CONTACT</Link>
-          <Link to="/">FAQ</Link>
-          <Link to="/">LOGIN</Link>
-          <Link to="/">SIGN UP</Link>
+          <Link to="/about">ABOUT</Link>
+          <Link to="/contact">CONTACT</Link>
+          <Link to="/faq">FAQ</Link>
+          <Link to="#">LOGIN</Link>
+          <Link to="#">SIGN UP</Link>
         </div>
 
         <div className={Styles.langBtns}>
