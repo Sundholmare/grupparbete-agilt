dae22e9041df0ea7c232369a9780eacb7d3c3e57
Marcus Järveskog <abr.metalcore98@gmail.com>
2021-09-02 16:46:39 +0200

    FAQ almoust done

--- src/components/Faq/index.js
+++ src/components/Faq/index.js
@@ -1,21 +1,18 @@
 import { useState } from 'react';
 import Styles from './faq.module.css';
+import Question from './Question';
 
 const Faq = () => {
 
   const answer = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci voluptatum nisi iure ratione hic. Distinctio eos possimus voluptatibus sed repellendus ea dolorem aperiam libero tempore repellat, ut ullam iusto dicta.'
   const questions = [
-    { title: 'Hur många knott finns det i ett knotthål?', answer: answer, id: 1 },
-    { title: 'Hur mår Tim idag, är han lite sjuk?', answer: answer, id: 2 },
-    { title: 'Jag vill verkligen spela idag, får jag göra det när jag är klar?', answer: answer, id: 3 },
-    { title: 'Kan du köpa en Monster energy till Mirwais?', answer: answer, id: 4 },
-    { title: 'Varför är Tara kvar i klassen?', answer: answer, id: 5 },
-    { title: 'Finns Tomten på riktigt, eller ljuger Filip för mig?', answer: answer, id: 6 },
+    { title: 'Lorem ipsum dolor, adipisicing elit. Recusandae, dolores?', answer: answer, id: 1 },
+    { title: 'Lorem ipsum dolor sit amet?', answer: answer, id: 2 },
+    { title: 'Lorem ipsum dolor sit dalia domor ipsa?', answer: answer, id: 3 },
+    { title: 'Random placeholder, kan du stå på 2 ben?', answer: answer, id: 4 },
+    { title: 'vem var när hur varför hallå?', answer: answer, id: 5 },
+    { title: 'Random ord som kan bli en fråga kanske?', answer: answer, id: 6 },
   ]
-
-  const [active, setActive] = useState(false);
-
-  const handelClick = () => setActive(!active)
 
   return (
     <div className={Styles.background}>
@@ -24,13 +21,7 @@ const Faq = () => {
           <h1 className={Styles.mainHeading}>FAQ's</h1>
           <div className={Styles.questions}>
             {questions.map((question) => (
-              <div onClick={handelClick} className={Styles.questionBox} key={question.id}>
-                <div className={Styles.qHeading}>
-                  <span className={Styles.icon}>+</span>
-                  <h3 className={Styles.question}>{question.title}</h3>
-                </div>
-                <p className={active ? `${Styles.answer} ${Styles.active}` : `${Styles.answer}`}>{question.answer}</p>
-              </div>
+              <Question title={question.title} answer={question.answer} key={question.id} />
             ))}
           </div>
         </div>
