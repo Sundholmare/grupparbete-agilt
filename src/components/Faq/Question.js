b67e3eb37d92bdd7a2634fb83cdc41d88d824546
Marcus Järveskog <abr.metalcore98@gmail.com>
2021-09-03 13:51:51 +0200

    finslipat animation på Faq

--- src/components/Faq/Question.js
+++ src/components/Faq/Question.js
@@ -1,5 +1,7 @@
 import { useState } from 'react';
 import Styles from './faq.module.css';
+import plus from '../../images/ikoner/plus-solid.svg'
+import minus from '../../images/ikoner/minus-solid.svg'
 
 const Question = (props) => {
 
@@ -9,15 +11,15 @@ const Question = (props) => {
     setActive(!active)
   }
 
-  return(
-      <div onClick={handelClick} className={Styles.questionBox}>
-        <div className={Styles.qHeading}>
-          <span className={Styles.icon}>+</span>
-          <h3 className={Styles.question}>{props.title}</h3>
-        </div>
-        <p className={active ? `${Styles.answer} ${Styles.active}` : `${Styles.answer}`}>{props.answer}</p>
+  return (
+    <div onClick={handelClick} className={Styles.questionBox}>
+      <div className={Styles.qHeading}>
+        <span className={Styles.icon}>{active ? <p>-</p> : <p>+</p>}</span>
+        <h3 className={Styles.question}>{props.title}</h3>
       </div>
-    )
+      <p className={active ? `${Styles.answer} ${Styles.active}` : `${Styles.answer}`}>{props.answer}</p>
+    </div>
+  )
 }
 
 export default Question
