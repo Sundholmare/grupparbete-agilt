ff1041485714e110e9e04542dd0749fd3c022a0a
Frida Waldt <70331019+FridaWaldt@users.noreply.github.com>
2021-09-03 14:08:41 +0200

    liten förändring

--- src/components/Hero/index.js
+++ src/components/Hero/index.js
@@ -5,7 +5,9 @@ const Hero = () => {
       <div className={Styles.hero}>
         <div className={Styles.heroBox}>
             <h1 className={Styles.heroTitle}>Your Abnormal Service Provider</h1>
-            <p className={Styles.heroText}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nec amet a et posuere tincidunt. Nisi eget nulla eu pellentesque. A in tristique ultrices a mi. Quis sit risus enim eget semper. A molestie tellus aliquam dolor non a viverra. Bibendum tincidunt tellus pulvinar orci. Faucibus elit sit velit tortor at nisi. Dui vel hac leo pretium, lacus, mattis magna. Ipsum lobortis dolor scelerisque ligula nisi nibh.</p>
+            <p className={Styles.heroText}> You won't have to look far to find just the service you are looking for. We provide a huge variety of services and no task is too abnormal for us to help with. 
+              
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nec amet a et posuere tincidunt. Nisi eget nulla eu pellentesque. A in tristique ultrices a mi. Quis sit risus enim eget semper. A molestie tellus aliquam dolor non a viverra. Bibendum tincidunt tellus pulvinar orci. Faucibus elit sit velit tortor at nisi. Dui vel hac leo pretium, lacus, mattis magna. Ipsum lobortis dolor scelerisque ligula nisi nibh.</p>
 
             <button className={Styles.heroButton}>Click here</button>
         </div>
